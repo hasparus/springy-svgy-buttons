bada187feb435e65eb69f7f29fbaaa4dd29401c2
hasparus <hasparus@gmail.com>
2018-06-05 14:12:56 +0200

    fix: Export types from package.

--- src/springy-svgy-buttons.ts
+++ src/springy-svgy-buttons.ts
@@ -1,4 +1,6 @@
 export {
   default as ButtonWithHoverCircle,
 } from './ButtonWithHoverCircle';
+export * from './ButtonWithHoverCircle';
 export { default as RippleButton } from './RippleButton';
+export * from './RippleButton';
