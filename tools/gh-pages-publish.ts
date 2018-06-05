c6f82b6cef39cd92ca77cc1f92bd30f6c85e3b68
hasparus <hasparus@gmail.com>
2018-06-05 14:20:58 +0200

    ci(travis): Add example deploy commands to travis.yml.

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -3,19 +3,6 @@ const { readFileSync } = require('fs');
 const url = require('url');
 const colors = require('colors');
 
-let repoUrl;
-let pkg = JSON.parse(readFileSync('package.json') as any);
-if (typeof pkg.repository === 'object') {
-  if (!pkg.repository.hasOwnProperty('url')) {
-    throw new Error('URL does not exist in repository section');
-  }
-  repoUrl = pkg.repository.url;
-} else {
-  repoUrl = pkg.repository;
-}
-
-let parsedUrl = url.parse(repoUrl);
-let repository = (parsedUrl.host || '') + (parsedUrl.path || '');
 let ghToken = process.env.GH_TOKEN || process.env.GITHUB_TOKEN;
 
 echo(colors.yellow('Deploying example!'));
@@ -26,5 +13,7 @@ exec('git add .');
 exec('git config user.name "hasparus"');
 exec('git config user.email "hasparus@gmail.com"');
 exec('git commit -m "docs(docs): update example on gh-pages"');
-exec(`git push --force --quiet "${repository}" master:gh-pages`);
+exec(
+  `git push --force --quiet https://"${ghToken}"@github.com/hasparus/springy-svgy-buttons master:gh-pages`
+);
 echo(colors.yellow('Example deployed!'));
